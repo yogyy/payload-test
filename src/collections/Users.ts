11850809880f7dd310fff0f74b6404368eb875fb
yogyy <yogimr261@gmail.com>
2024-03-16 23:59:02 +0700

    chore: update table users

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,13 +2,20 @@ import { CollectionConfig } from "payload/types";
 
 const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    useAPIKey: true,
+  },
   admin: {
-    useAsTitle: "email",
+    useAsTitle: "username",
   },
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: "username",
+      type: "text",
+      required: true,
+      unique: true,
+      saveToJWT: true,
+    },
   ],
 };
 
